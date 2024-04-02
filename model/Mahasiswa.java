0f77824dc24848ff4abc001ac7878e018a14d3e8
muhammadriyadi <riyadi1124543@gmail.com>
2024-04-02 20:42:09 +0800

    Buat Class Main dan Database

---
+++ model/Mahasiswa.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Dosen{
+public static void tampilanInfo(){
+System.out.println();
+System.out.println("==Data Mahasiswa================");
+System.out.println("NIDN : 2210010002");
+System.out.println("Nama : Muhammad Riyadi");
+System.out.println("============================");
+}
+}
